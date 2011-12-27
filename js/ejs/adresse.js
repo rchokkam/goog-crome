d1af7e88d2681a39eff37cec331e34fa25625a76
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-12-27 15:40:11 +0100

    Fix address etage issue

--- js/ejs/adresse.js
+++ js/ejs/adresse.js
@@ -1,2 +1,2 @@
-<div id="kaddress" style='display:none'><%= vejnavn %> <%= husnr %> <%= etage %> <%= doernr %>, <%= postnr %> <%= kommune %></div>
+<div id="kaddress" style='display:none'><%= vejnavn %> <%= husnr %>, <%= postnr %> <%= kommune %></div>
 <div id="map_canvas" style="width: 860px; height: 650px"></div>
