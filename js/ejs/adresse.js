23355f45e6dd93f2ef8beeb6d428f622d4341b89
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-12-27 15:44:44 +0100

    formatting

--- js/ejs/adresse.js
+++ js/ejs/adresse.js
@@ -1,2 +1,4 @@
-<div id="kaddress" style='display:none'><%= vejnavn %> <%= husnr %>, <%= postnr %> <%= kommune %></div>
+<div id="kaddress" style='display:none'>
+<%= vejnavn %> <%= husnr %>, <%= postnr %> <%= kommune %>
+</div>
 <div id="map_canvas" style="width: 860px; height: 650px"></div>
